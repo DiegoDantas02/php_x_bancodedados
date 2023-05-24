471abeef36e47e63ec8872826b219f0f27398f11
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2023-05-24 19:36:50 -0300

    Create ativone.php

---
+++ ativone.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>atividade 01</title>
+
+</head>
+
+<body>
+
+    //Escreva um programa em PHP que declare uma variável chamada "nome" e atribua a ela o valor "Maria". Em seguida, imprima a mensagem "Olá, Maria!".
+
+<?php
+$nome = "Maria";
+echo "Olá. $nome!";
+?>
+</body>
+
+</html>
