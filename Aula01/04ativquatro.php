e2c203bad1861c850e09ce85a1f69145f9628bfe
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2023-05-25 20:37:12 -0300

    .

---
+++ Aula01/04ativquatro.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title> Atividade 04</title>
+
+</head>
+
+<body>
+    <?php
+    //Crie uma variável chamada "numero" e atribua a ela um valor. Verifique se o número é par ou ímpar e exiba uma mensagem correspondente. 
+    $numero = 5;
+    if($numero / $numero){
+        echo "O Numero Selecionado é : " . $numero . " Par";
+    }else{
+        echo "O Numero Selecionado é: " . $numero . " Impar";
+    }
+    ?>
+</body>
+
+</html>
