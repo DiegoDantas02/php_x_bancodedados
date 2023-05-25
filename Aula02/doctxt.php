9cd325c9c87961c179eba4e5037d923c29722cf6
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2023-05-24 22:19:14 -0300

    ATT

---
+++ Aula02/doctxt.php
@@ -0,0 +1,18 @@
+<?php
+//operadores relacionais
+
+echo (20 > 30) . "FALSE<br>"; //maior
+echo (20 < 30) . "TRUE<br>"; //menor
+echo (10 == 10) . "TRUE<br>"; //igualdade não mostra 
+echo (20 != 30) . "FALSE<br>"; //diferente
+echo (20 >= 30) . "TRUE<br>"; //maior ou igual
+echo (20 <= 30) . "TRUE<br>"; //menor ou igual
+
+//operadores logicos
+
+echo (10 == 10 && 10 > 5) . "TRUE<br>"; //operador E
+echo (10 == 10 || 10 > 5) . "TRUE<br>"; //operador OU
+echo (!false) . "TRUE<br>"; //operador NÃO
+
+
+?>
