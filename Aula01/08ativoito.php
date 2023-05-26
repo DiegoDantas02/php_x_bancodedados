c557f40718afa83713b9442b831c7b10e402234d
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2023-05-25 21:52:57 -0300

    .

---
+++ Aula01/08ativoito.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Atividade 08</title>
+
+</head>
+<body>
+    
+    <?php
+    echo "<h1>Crie duas variáveis numéricas e realize a soma entre elas. Em seguida, imprima o resultado. </h1>";
+    $valor = 2;
+    $valor1 = 4;
+
+    $valor + $valor1;
+    echo 
+
+    ?>
+
+</body>
+</html>
