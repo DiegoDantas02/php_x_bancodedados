f68d09844b41098fc5c46d9315d738dba81e9b7d
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2023-05-25 21:13:06 -0300

    .

---
+++ Aula01/05ativcinco.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title> Atividade 05</title>
+
+</head>
+
+<body>
+
+
+    <?php
+
+    echo "<h1>Escreva um programa que converta uma temperatura em graus Celsius para Fahrenheit.<br> A fórmula de conversão é: Fahrenheit = (Celsius * 9/5) + 32.</h1>";
+
+    $celsius = 32;
+    $fahrenheit = $celsius * 9 / 5 + 32;
+    echo $celsius . " graus Celsius é igual a " . $fahrenheit . " graus Fahrenheit.";
+
+
+    ?>
+
+</body>
+
+</html>
