993638f2282969d009214cc9b6732c1fb3570143
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2023-05-30 19:57:07 -0300

    .

--- Aula02_formularios/Ativ02.php
+++ Aula02_formularios/Ativ02.php
@@ -9,23 +9,31 @@
     <title>Atividade 02</title>
 
 </head>
-
 <body>
 
     <p>Escreva um programa que receba os dados de um formulário HTML contendo os campos "idade" e "altura". Valide se os campos foram preenchidos corretamente e exiba uma mensagem com os valores informados</p>
 
     <form>
-        Digite Sua Idade:
-        <input type="idade"><br> <br>
-        Digite Sua altura:
-        <input type="altura"> <br> <br>
-        <button type="submit">Enviar</button>
-    </form>
-    <?php
 
+       <h1> Digite Sua Idade:</h1>
+
+        <input type="idade" name="idade"><br> <br>
+        <h1>Digite Sua altura:</h1>
+
+        <input type="altura" name="altura"> <br> <br>
+        <button type="submit">Enviar</button> 
+        <button type="submit">Limpar</button>
+
+    </form>
+    
+    <?php
+    $altura = $_GET["altura"];
+    echo "<h1>Sua Altura é:</h1>" . "<h2>$altura</h2> <br>"; 
+
+    $idade = $_GET["idade"];
+    echo "<h1>Sua idade é: </h1>" . "<h2>$idade</h2><br>";
 
     ?>
 
 </body>
-
 </html>
