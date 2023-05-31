10f795bbe9789a3537d3b71c71c68f4bb69966dc
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2023-05-31 20:28:06 -0300

    .

--- Ativ03 array/Ativ07.php
+++ Ativ03 array/Ativ07.php
@@ -1,1 +1,22 @@
-Crie um array de strings contendo o nome de 5 países diferentes. Em seguida, percorra o array e imprima cada país em uma linha separada. 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Crie um array de strings contendo o nome de 5 países diferentes. Em seguida, percorra o array e imprima cada país em uma linha separada. </p>
+
+    <?php
+    $pais = array("EUA", "CANADA","ITALIA", "ESPANHA","PORTUGAL");
+    foreach($pais as $pais){
+    echo $pais . "<br>";}
+   
+    
+    ?>
+
+
+</body>
+</html>
--- Ativ03 array/Ativ08.php
+++ Ativ03 array/Ativ08.php
@@ -1,1 +1,23 @@
-Crie um array numérico com 5 números aleatórios. Em seguida, ordene o array em ordem decrescente e imprima o resultado. 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<p>Crie um array numérico com 5 números aleatórios. Em seguida, ordene o array em ordem decrescente e imprima o resultado.</p>    
+
+<?php
+    $numero = array("222", "27", "835", "474", "58");
+    echo "<br>";
+    rsort($numero);
+    print_r($numero);
+    
+    ?>
+
+
+</body>
+</html>
+ 
