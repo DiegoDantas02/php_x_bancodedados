05198dfe5e499a02927571b09dbaf06a63d90b73
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2023-05-31 21:13:51 -0300

    .

--- Aula02_formularios/Ativ05.php
+++ Aula02_formularios/Ativ05.php
@@ -12,11 +12,39 @@
 
 <body>
 
-    <p>Crie um formulário que solicite ao usuário uma série de números separados por vírgula. Ao enviar o formulário, separe os números em um array em PHP e exiba a soma de todos os números.</p>
+    <!-- <p>Crie um formulário que solicite ao usuário uma série de números separados por vírgula. Ao enviar o formulário, separe os números em um array em PHP e exiba a soma de todos os números.</p>
+<FORM>
+    <h1>Digite uma serie de Numeros com virgulas</h1>
+    <input name="number">
+    <button type="submit">Enviar</button>
+</FORM>
 
     <?php
     
-    ?>
+    ?> -->
+    <?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $numeros = $_POST['numeros'];
+    $arrayNumeros = explode(',', $numeros);
+    $soma = array_sum($arrayNumeros);
+    echo "A soma dos números é: " . $soma;
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Formulário de Números</title>
+</head>
+<body>
+    <form method="POST" action="">
+        <label for="numeros">Digite uma série de números separados por vírgula:</label><br>
+        <input type="text" name="numeros" id="numeros"><br><br>
+        <input type="submit" value="Calcular Soma">
+    </form>
+</body>
+</html>
+
 
 </body>
 </html>
