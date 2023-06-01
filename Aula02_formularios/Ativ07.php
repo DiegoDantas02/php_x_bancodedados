a35cbf79bb6f701f392f5b7844a7a63a3a1099d4
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2023-06-01 20:55:39 -0300

    .

--- Aula02_formularios/Ativ07.php
+++ Aula02_formularios/Ativ07.php
@@ -14,26 +14,38 @@
 <body>
 
     <P>Crie um formulário que solicite ao usuário uma lista de compras,
-     onde cada item é inserido em um campo de texto separado. Ao enviar o formulário, exiba a lista de compras completa. </P>
+        onde cada item é inserido em um campo de texto separado. Ao enviar o formulário, exiba a lista de compras completa. </P>
 
-    
+
+
+        <!DOCTYPE html>
+<html>
+<head>
+    <title>Formulário de Lista de Compras</title>
+</head>
+<body>
 
 <h2>Lista de Compras</h2>
 
+<!-- server[PHP_SELF] serve para reenviar o formulario para a propria pagina -->
 <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <!-- Campos de texto para itens de compra -->
     <input type="text" name="items[]" required><br>
     <input type="text" name="items[]" required><br>
     <input type="text" name="items[]" required><br>
-
-    <!-- Adicione mais campos de item aqui, se necessário -->
-
+<!-- botão de enviar o conteudo -->
     <input type="submit" value="Enviar">
 </form>
 
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Recupera os valores dos campos de texto e armazena no array $items
     $items = $_POST['items'];
+
+    // Exibe o título "Lista de Compras:"
     echo "<h2>Lista de Compras:</h2>";
+
+    // Exibe os itens da lista de compras em uma lista não ordenada
     echo "<ul>";
     foreach ($items as $item) {
         echo "<li>" . htmlspecialchars($item) . "</li>";
