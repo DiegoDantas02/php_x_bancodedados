33932e6f45c888553053a57b0f36d6edb51f4df4
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2023-06-01 20:56:37 -0300

    .

--- Aula02_formularios/Ativ07.php
+++ Aula02_formularios/Ativ07.php
@@ -18,41 +18,45 @@
 
 
 
-        <!DOCTYPE html>
-<html>
-<head>
-    <title>Formulário de Lista de Compras</title>
-</head>
-<body>
+    <!DOCTYPE html>
+    <html>
 
-<h2>Lista de Compras</h2>
+    <head>
+        <title>Formulário de Lista de Compras</title>
+    </head>
 
-<!-- server[PHP_SELF] serve para reenviar o formulario para a propria pagina -->
-<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-    <!-- Campos de texto para itens de compra -->
-    <input type="text" name="items[]" required><br>
-    <input type="text" name="items[]" required><br>
-    <input type="text" name="items[]" required><br>
-<!-- botão de enviar o conteudo -->
-    <input type="submit" value="Enviar">
-</form>
+    <body>
 
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Recupera os valores dos campos de texto e armazena no array $items
-    $items = $_POST['items'];
+        <h2>Lista de Compras</h2>
 
-    // Exibe o título "Lista de Compras:"
-    echo "<h2>Lista de Compras:</h2>";
+        <!-- server[PHP_SELF] serve para reenviar o formulario para a propria pagina -->
+        <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+            <!-- Campos de texto para itens de compra -->
+            <input type="text" name="items[]" required><br>
+            <input type="text" name="items[]" required><br>
+            <input type="text" name="items[]" required><br>
+            <!-- botão de enviar o conteudo -->
+            <input type="submit" value="Enviar">
+        </form>
 
-    // Exibe os itens da lista de compras em uma lista não ordenada
-    echo "<ul>";
-    foreach ($items as $item) {
-        echo "<li>" . htmlspecialchars($item) . "</li>";
-    }
-    echo "</ul>";
-}
-?>
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            // Recupera os valores dos campos de texto e armazena no array $items
+            $items = $_POST['items'];
 
-</body>
-</html>
+            // Exibe o título "Lista de Compras:"
+            echo "<h2>Lista de Compras:</h2>";
+
+            // Exibe os itens da lista de compras em uma lista não ordenada
+            echo "<ul>";
+            foreach ($items as $item) {
+                //A função htmlspecialchars é usada para garantir que os itens sejam exibidos corretamente no HTML.
+                echo "<li>" . htmlspecialchars($item) . "</li>";
+            }
+            echo "</ul>";
+        }
+        ?>
+
+    </body>
+
+    </html>
