3273fd204077b06fd6113f20be16209d0b1355ca
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2023-06-07 22:20:58 -0300

    .

--- Aula06-software/pastas de redirecionamentos/novo-salario.php
+++ Aula06-software/pastas de redirecionamentos/novo-salario.php
@@ -5,13 +5,14 @@ include "cabecalho.php";
 <!-- --------------- -->
 <br>
 <main class="container">
-    <form class="salvar-salario.php" method="post">
-    <label>Nome: <input name="nome"></label>
-    <label>Sobrenome: <input name="sobrenome"></label>
+    <form action="salvar-salario.php" method="post" >
+    <label>Data Da compra: <input date="data"></label>
+    <label>Nome Da companhia: <input name=""></label>
     <button type="submit">SALVAR</button>
     </form>
 </main>
 <br>
+<!--  -->
 <?php
 include "rodape.php" ;
 ?>
--- Aula06-software/pastas de redirecionamentos/salvar-salario.php
+++ Aula06-software/pastas de redirecionamentos/salvar-salario.php
@@ -18,20 +18,14 @@ $conexao = mysqli_connect($bd_servidor, $bd_usuario, $bd_senha, $bd_nome_do_banc
 
 
 //criar o sql inserir
-$sql = "insert into t_cliente(nome, sobrenome) values('$nome', '$sobrenome')";
-
-echo "$sql";
-
-
+$sql = "insert into t_cliente(data, nome_da_empresa) values('$data', '$nome_da_empresa')";
 
 //executar o sql no banco
-
-
-
+mysqli_query($conexao, $sql);
 
 //fechar a conexão
-
+mysqli_close($conexao);
 
 //redirecionar para a página de listagem
-
+header('location:salario.php');
 ?>
