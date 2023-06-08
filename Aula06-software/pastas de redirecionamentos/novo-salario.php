98bff30a5da1cd85c37b71a0aaa5256cf781f78b
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2023-06-07 21:32:39 -0300

    .

--- Aula06-software/pastas de redirecionamentos/novo-salario.php
+++ Aula06-software/pastas de redirecionamentos/novo-salario.php
@@ -3,15 +3,15 @@
 include "cabecalho.php";
 ?>
 <!-- --------------- -->
-
+<br>
 <main class="container">
-    <form class="../pastas de redirecionamentos/salario-salvar.php" method="post">
-    <input name="nome">
-    <input name="sobrenome">
+    <form class="salvar-salario.php" method="post">
+    <label>Nome: <input name="nome"></label>
+    <label>Sobrenome: <input name="sobrenome"></label>
     <button type="submit">SALVAR</button>
     </form>
 </main>
-
+<br>
 <?php
 include "rodape.php" ;
 ?>
--- Aula06-software/pastas de redirecionamentos/salvar-salario.php
+++ Aula06-software/pastas de redirecionamentos/salvar-salario.php
@@ -18,7 +18,9 @@ $conexao = mysqli_connect($bd_servidor, $bd_usuario, $bd_senha, $bd_financa);
 
 
 //criar o sql inserir
+$sql = "insert into t_cliente(nome, sobrenome) values('$nome', '$sobrenome')";
 
+echo $sql;
 
 
 
